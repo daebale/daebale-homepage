3468012774e7621e54fec75d7b8ee853040d5814
daebale <ylyl0877@naver.com>
2025-05-10 05:22:36 +0900

    Add files via upload

---
+++ src/DaebalHome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DaebalHome() {
+  return (
+    <div className="text-center p-12 text-blue-700 font-bold text-2xl">
+      대리발송 대발이 홈페이지 최종 완성본 (디자인 + 텍스트 포함)
+    </div>
+  );
+}
